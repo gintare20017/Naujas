6393650eafe78e1101be7b2182ee2d0180073c9c
gintare20017 <gintare20017@gmail.com>
2021-12-09 09:05:14 +0200

    pakoreguotas scriptas

--- naujas6.sh
+++ naujas6.sh
@@ -3,14 +3,32 @@ echo "Iveskite slaptazodi"
 read Ivestas_slaptazodis
 eilute1="psw123"
 if [ "$eilute1" == "$Ivestas_slaptazodis" ]
-   then
-       echo "Iveskite failo pavadinima"
-       read failas
+then
+    echo "Iveskite failo pavadinima"
+    read failas
+    if [ -e "$failas" ]
+    then 
+        echo "$failas egzistuoja, ar norite ji istrinti ir sukurti nauja?"
+        read pasirinkimas
+        eilute2="taip"
+        eilute3="ne"
+        if [ "$pasirinkimas" == "$eilute2" ]
+        then
+            rm -f "$failas"
+            echo "$failas yra istrintas"
+            touch "$failas"
+            echo "sukurtas naujas $failas"
+       	else 
+            echo "$failas buvo nesukurtas"
+	fi
+    else 
+	touch "$failas"
+	echo "Toks $failas buvo sukurtas"
+    fi
+else
+    echo "Neteisingas slaptazodis"    
 fi
-if [ -e "$failas" ]
-   then 
-       echo "$failas egzistuoja"
-   else 
-       touch "$failas"
-       echo "Failas yra sukurtas"
-fi
+
+
+
+
